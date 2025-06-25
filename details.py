617d93742beb6d474c3c2d75b6beebbddfbe2def
salman1998 <salmankhosa1998@gmail.com>
2025-06-25 14:51:00 +0500

    agged.txt

---
+++ details.py
@@ -0,0 +1,17 @@
+num1 = input("Enter first num: ")
+num2 = input("Enter second num: ")
+
+sum_of_num = int(num1) + int(num2)
+
+print("The sum of", num1, "and", num2, "is:", sum_of_num)
+
+print ( "sum of 2 numbers: " , sum_of_num)
+      
+print("diff of 2 numbers:", int(num1) - int(num2))
+
+sum_of_num = int(num1) * int(num2)
+print("product of 2 numbers:", sum_of_num)
+
+diff_of_num = int(num1) / int(num2)
+print("division of 2 numbers:", diff_of_num)
+
